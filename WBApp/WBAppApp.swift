7941fa51dcb5edfe380c5286a5d3dc15f36c9e53
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-06-06 18:11:45 +0300

    WalkthroughScreen done

---
+++ WBApp/WBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBAppApp.swift
+//  WBApp
+//
+//  Created by Денис Карпов on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBAppApp: App {
+	var body: some Scene {
+		WindowGroup {
+			WalkthroughScreen()
+		}
+	}
+}
---
+++ WBApp/Walkthrough/WalkthroughScreen.swift
@@ -0,0 +1,32 @@
+//
+//  Onboarding.swift
+//  WBApp
+//
+//  Created by Денис Карпов on 05.06.2024.
+//
+
+import SwiftUI
+
+struct WalkthroughScreen: View {
+	
+	@State var verificationViewPresented = false
+	
+	var body: some View {
+			VStack {
+				Spacer()
+				WalkthroughScreen_LabelView()
+				Spacer()
+				WalkthroughScreen_ButtonStackView {
+					verificationViewPresented.toggle()
+				}
+			}
+			.padding(.vertical, 20)
+			.sheet(isPresented: $verificationViewPresented, content: {
+				VerificationScreen()
+			})
+	}
+}
+
+#Preview {
+	WalkthroughScreen()
+}
